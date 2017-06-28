e9f3368403ae1fdd92fffa4eca14a8d0f960f1a6
zhangleilei <zhangleilei@mogu61.com>
2017-06-28 18:27:54 +0800

    123

--- FE/demo/demo1/renderEngine.js
+++ FE/demo/demo1/renderEngine.js
@@ -1,29 +1,52 @@
-class RenderEngine(){
+class RenderEngine{
     constructor(){
-        this.canvasEle = document.querySelector('canvas');
-        this.context = this.canvasEle.getContext('2d');
+        //获取canvas环境&贴图等初始化
+        this.init();
         //坦克列表
         this.tankList = [];
         //生成坦克实例
-        initTank();
+        this.initTank();
         //绑定事件
-        bindEvent();
+        this.bindEvent();
         //开始渲染
-        startRender();
+        this.startRender();
+    }
+    init(){
+        let canvasEle = document.querySelector('canvas');
+        this.context = canvasEle.getContext('2d');
+        this.pic = new Image()
+        this.pic.src = '../tanks_sheet.png';
     }
     initTank(){
-        this.tankList.push(new Tank());
+        this.tankList.push(new Tank(50,50));
     }
     bindEvent(){
-        this.canvasEle.addEventListener('keydown',(e)=>{
-            console.log(e.keyCode)
-        })
+        window.addEventListener('keypress',()=>{
+            console.log(1)
+            // todo 根据上下左右调用tank go 方法
+        },false)
     }
     startRender(){
-
+        this.render()
+        window.requestAnimationFrame(this.startRender.bind(this));
     }
     render(){
-
+        let {context,pic} = this;
+        context.fiiStyle ='#aaa';
+        context.fillRect(0,0,600,600);
+        this.tankList.forEach((el)=>{
+            let {face,positionX,positionY,frameIndex} =el,
+            faceList = ['front','left','right','back'],
+            animationFrames = [1,2,3,4,5,6,7,8],
+            sourceX = Math.floor(animationFrames[frameIndex] % 8)*32,
+            sourceY = Math.floor(animationFrames[frameIndex] /8)*32;
+            context.save()
+            context.translate(positionX+16,positionY+16);
+            console.log(faceList.indexOf(face)*Math.PI/4)
+            context.rotate(faceList.indexOf(face)*Math.PI/4)
+            context.drawImage(pic,sourceX,sourceY,32,32,-16,-16,32,32)
+            context.restore()
+        })
     }
 }
-RenderEngine()
+new RenderEngine()
--- FE/demo/demo1/tankClass.js
+++ FE/demo/demo1/tankClass.js
@@ -1,8 +1,8 @@
-window.Tank = class Tank(){
+window.Tank = class Tank{
     constructor(x,y,face='front'){
         //初始位置
         this.positionX = x;
-        this.positionX = y;
+        this.positionY = y;
         //朝向：front back left right
         this.face = face ;
         //帧位置，显示不同的贴图
@@ -11,4 +11,7 @@ window.Tank = class Tank(){
         this.dx = 1;
         this.dy = 1;
     }
+    // go(font){
+    //     改变实例的参数
+    // }
 }
